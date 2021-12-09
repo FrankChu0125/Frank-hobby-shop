74cf7a1764271b7972e337bf289012c543e4fee6
FrankChu <frankchu@ifalo.com.tw>
2021-12-09 17:43:56 +0800

    feat(header): 拆解出header區域

--- hobby-shop/src/App.vue
+++ hobby-shop/src/App.vue
@@ -1,22 +1,21 @@
+  
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header></Header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Microsoft JhengHei";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
